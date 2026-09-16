306c508e125ce1e98eb265f4ce9578d45fbcccbf
marko-preo <marko.preocanin@students.mq.edu.au>
2026-09-16 11:10:44 +1000

    Exercise 5.7 completed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = {
+    display: visible ? 'none' : ''
+  }
+
+  const showWhenVisible = {
+    display: visible ? '' : 'none'
+  }
+
+  return (
+    <div>
+      <div>
+        {blog.title} {blog.author}
+
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
